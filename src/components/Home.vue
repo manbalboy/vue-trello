0e0e339836aa3397d3e9e3da56cb5137ec78e36b
manbalboy <manbalboy@hanmail.net>
2021-02-25 08:39:28 +0900

    HttpRequest를 이용한 서버 통신

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
         Home
         <div>
             Board List :
+            <div v-if="isLoading">Loading ....</div>
+            <div v-else>Api result :{{ apiRes }}</div>
             <ul>
                 <li>
                     <router-link to="/b/1">Board 1</router-link>
@@ -16,7 +18,35 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            isLoading: true,
+            apiRes: '',
+        };
+    },
+
+    created() {
+        this.fetchData();
+    },
+
+    methods: {
+        fetchData() {
+            this.isLoading = true;
+            const req = new XMLHttpRequest();
+            req.open('GET', 'http://localhost:3000/health');
+            req.send();
+            req.addEventListener('load', () => {
+                this.isLoading = false;
+                this.apiRes = {
+                    status: req.status,
+                    statusText: req.statustText,
+                    response: JSON.parse(req.response),
+                };
+            });
+        },
+    },
+};
 </script>
 
 <style></style>
