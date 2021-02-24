c164946cad8628055ca9144eb398cc4db3e78dd1
manbalboy <manbalboy@hanmail.net>
2021-02-24 23:40:54 +0900

    NaviBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <h1>APP</h1>
+        <NaviBar></NaviBar>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import NaviBar from '@/components/NaviBar.vue';
 export default {
     data() {
         return {};
     },
+
+    components: { NaviBar },
 };
 </script>
 
