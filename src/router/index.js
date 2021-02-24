1719f2d33a4a0385d195bbac7822e8f9daf712f4
manbalboy <manbalboy@hanmail.net>
2021-02-24 23:14:00 +0900

    라우터 소스 정리

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
     { path: '', redirect: { name: 'home' } },
     { path: '/home', name: 'home', component: { template: '<div>foo</div>' } },
-    // { path: '/about', name: 'about', component: About },
-    // { path: '/todos', name: 'todoList', component: TodoList },
-    // { path: '/todos/add', name: 'addTodo', component: AddTodo },
-    // { path: '/todos/update/:id', name: 'updateTodo', component: UpdateTodo },
     {
+        // vue 2에서는 '*' 만 해도 됨 vue 3 에서는 다음과 같이 동작
         path: '/:catchAll(.*)',
         name: 'NotFound',
         component: { template: '<div>notfound</div>' },
