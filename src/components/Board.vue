cdb30fe1c4cd0a766444963a88e4d0f5ab2c47e9
manbalboy <manbalboy@hanmail.net>
2021-02-25 00:06:53 +0900

    동적 라우트 매칭

---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Board : bid = {{ bid }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bid: 0,
+        };
+    },
+
+    created() {
+        this.bid = this.$route.params.bid;
+    },
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/Home.vue';
 import Login from '@/components/Login.vue';
 import NotFound from '@/components/NotFound.vue';
+import Board from '@/components/Board.vue';
 
 const routes = [
     { path: '', redirect: { name: 'home' } },
     { path: '/home', name: 'home', component: Home },
     { path: '/login', name: 'login', component: Login },
+    { path: '/b/:bid', name: 'login', component: Board },
     {
         // vue 2에서는 '*' 만 해도 됨 vue 3 에서는 다음과 같이 동작
         path: '/:catchAll(.*)',
