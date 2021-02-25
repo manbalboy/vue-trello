132b8bd9bad56ea249c91b5731caa629ff7bc0c2
manbalboy <manbalboy@hanmail.net>
2021-02-25 21:53:02 +0900

    네비게이션가드 등록

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,36 @@ import NotFound from '@/components/NotFound.vue';
 import Board from '@/components/Board.vue';
 import Card from '@/components/Card.vue';
 
+//네이게이션가드
+const requireAuth = (to, from, next) => {
+    const isAuth = localStorage.getItem('token');
+    const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+
+    isAuth ? next() : next(loginPath);
+};
+
 const routes = [
     { path: '', redirect: { name: 'Home' } },
-    { path: '/home', name: 'Home', component: Home },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        beforeEnter: requireAuth,
+    },
     { path: '/login', name: 'Login', component: Login },
     {
         path: '/b/:bid',
         name: 'Board',
         component: Board,
-        children: [{ path: 'c/:cid', name: 'Card', component: Card }],
+        beforeEnter: requireAuth,
+        children: [
+            {
+                path: 'c/:cid',
+                name: 'Card',
+                component: Card,
+                beforeEnter: requireAuth,
+            },
+        ],
     },
 
     {
