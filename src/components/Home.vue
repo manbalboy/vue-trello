cc32f1a8c93201cb905d1e757d6614d3423d0e37
manbalboy <manbalboy@hanmail.net>
2021-02-26 02:18:04 +0900

    composition 삽질..

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,72 +1,124 @@
 <template>
     <div>
-        Home
-        <div>
-            Board List :
-            <div v-if="isLoading">Loading ....</div>
-            <div v-else>
-                <div v-for="b in boards" :key="b.id">
-                    {{ b }}
-                </div>
+        <div class="home-title">Personal Boards</div>
+        <div class="board-list" ref="boardList">
+            <div
+                class="board-item"
+                v-for="b in boards"
+                :key="b.id"
+                :data-bgcolor="b.bgColor"
+                :ref="setItemRef"
+            >
+                <router-link :to="`/b/${b.id}`">
+                    <div class="board-item-title">{{ b.title }}</div>
+                </router-link>
             </div>
-            <ul>
-                <li>
-                    <router-link to="/b/1">Board 1</router-link>
-                </li>
-                <li>
-                    <router-link to="/b/1">Board 2</router-link>
-                </li>
-            </ul>
+            <div class="board-item board-item-new">
+                <a class="new-board-btn" href="" @click.prevent="addBoard">
+                    Create new board...
+                </a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { board, setAuthInHeader } from '@/api/index.js';
+import { board } from '../api';
 export default {
-    data() {
+    setup() {
+        let itemRefs = [];
+        const setItemRef = el => {
+            if (el) {
+                itemRefs.push(el);
+            }
+        };
+
         return {
-            isLoading: true,
-            boards: '',
-            err: '',
+            setItemRef,
+            itemRefs,
         };
     },
-
+    data() {
+        return {
+            loading: false,
+            boards: [],
+            error: '',
+        };
+    },
     created() {
-        const { token } = localStorage;
-        if (token) {
-            setAuthInHeader(token);
-        }
         this.fetchData();
     },
+    updated() {
+        console.log('fda', this.itemRefs);
 
+        // this.boardItem.style.backgroundColor = this.boardItem.dataset.bgcolor;
+        this.itemRefs.forEach(el => {
+            el.style.backgroundColor = el.dataset.bgcolor;
+        });
+    },
     methods: {
         fetchData() {
-            this.isLoading = true;
-
+            this.loading = true;
             board
                 .fetch()
-                .then(response => {
-                    this.boards = response;
+                .then(data => {
+                    this.boards = data.list;
                 })
                 .finally(_ => {
-                    this.isLoading = false;
+                    this.loading = false;
                 });
-
-            // const req = new XMLHttpRequest();
-            // req.open('GET', 'http://localhost:3000/health');
-            // req.send();
-            // req.addEventListener('load', () => {
-            //     this.isLoading = false;
-            //     this.apiRes = {
-            //         status: req.status,
-            //         statusText: req.statustText,
-            //         response: JSON.parse(req.response),
-            //     };
-            // });
+        },
+        addBoard() {
+            console.log('addBoard()');
         },
     },
 };
 </script>
 
-<style></style>
+<style>
+.home-title {
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+}
+.board-list {
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.board-item {
+    width: 23%;
+    height: 100px;
+    margin: 0 2% 20px 0;
+    border-radius: 3px;
+}
+.board-item-new {
+    background-color: #ddd;
+}
+.board-item a {
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+    background-color: rgba(0, 0, 0, 0.1);
+    color: #666;
+}
+.board-item-title {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 700;
+    padding: 10px;
+}
+.board-item a.new-board-btn {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    height: 100px;
+    width: inherit;
+    color: #888;
+    font-weight: 700;
+}
+</style>
