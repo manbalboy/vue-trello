ae7ac7ca81dc36180ebe2a05f3c4989a3f8f6a29
manbalboy <manbalboy@hanmail.net>
2021-02-27 21:12:02 +0900

    카드이동구현

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -26,23 +26,76 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 import List from '@/components/List.vue';
+import dragula from 'dragula';
+import 'dragula/dist/dragula.css';
 
 export default {
     data() {
         return {
             bid: 0,
             loading: false,
+            dragulaCards: null,
         };
     },
     components: { List },
     created() {
         this.fetchData();
     },
+    updated() {
+        if (this.dragulaCards) {
+            this.dragulaCards.destroy();
+        }
+        this.dragulaCards = dragula([
+            ...Array.from(this.$el.querySelectorAll('.card-list')),
+        ]).on('drop', (el, wrapper, target, siblings) => {
+            console.log('test', el, wrapper, target, siblings);
+
+            const targetCard = {
+                id: el.dataset.cardId * 1,
+                pos: 65335,
+            };
+            let preCard = null;
+            let nextCard = null;
+
+            Array.from(wrapper.querySelectorAll('.card-item')).forEach(
+                (el, idx, arr) => {
+                    const cardId = el.dataset.cardId * 1;
+                    if (cardId == targetCard.id) {
+                        preCard =
+                            idx > 0
+                                ? {
+                                      id: arr[idx - 1].dataset.cardId * 1,
+                                      pos: arr[idx - 1].dataset.cardPos * 1,
+                                  }
+                                : null;
+                        nextCard =
+                            idx < arr.length - 1
+                                ? {
+                                      id: arr[idx + 1].dataset.cardId * 1,
+                                      pos: arr[idx + 1].dataset.cardPos * 1,
+                                  }
+                                : null;
+                    }
+                },
+            );
+            if (!preCard && nextCard) {
+                targetCard.pos = nextCard.pos / 2;
+            } else if (preCard && !nextCard) {
+                targetCard.pos = preCard.pos * 2;
+            } else if (preCard && nextCard) {
+                targetCard.pos = (preCard.pos + nextCard.pos) / 2;
+            }
+
+            console.log(targetCard);
+            this.UPDATE_CARD(targetCard);
+        });
+    },
     computed: {
         ...mapState(['board']),
     },
+
     methods: {
-        ...mapActions(['FETCH_BOARD']),
+        ...mapActions(['FETCH_BOARD', 'UPDATE_CARD']),
         fetchData() {
             this.loading = true;
             this.FETCH_BOARD({ id: this.$route.params.bid }).then(() => {
